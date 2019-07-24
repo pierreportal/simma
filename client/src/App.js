ccf14b657e2d6b0aeb179ea9b35f44c18da5f155
pierre portal <pierreportal.mac@gmail.com>
2019-07-24 17:14:40 +0200

    client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Final project 💀☠️💀☠️</h1>
+    </div>
+  );
+}
+
+export default App;
